d49982ae92a88cfc9252f50562e0abb21e6263df
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-08-05 23:09:10 +0200

    add gallery html and js files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-console.log('helle')
+const containerGallery = document.querySelector('div.gallery');
+
+const itemsTemplate = ({ original, preview, description }) =>
+  `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image"
+        src="${preview}"
+        alt="${description}">
+    </a>`;
+const сreateList = () => {
+  const galleryItem = galleryItems.map(itemsTemplate).join('');
+  containerGallery.insertAdjacentHTML('beforeend', galleryItem);
+};
+
+сreateList();
+
+const gallery = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
