7834df924e82065398a484fb4a7dd454932e8fef
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-08-07 16:00:50 +0200

    add files for video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log('VIDEO')
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
