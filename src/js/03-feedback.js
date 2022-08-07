e92eb26df2717885c254f46902953676430ae25c
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-08-07 18:39:37 +0200

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
-console.log('feedback');
+import throttle from 'lodash.throttle';
+
+const formElement = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let formValue = {};
+
+formElement.addEventListener('input', throttle(saveDataForm, 1000));
+formElement.addEventListener('submit', handlerSubmit);
+
+function saveDataForm(e) {
+  formValue[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function showDataForm() {
+  const localStorageValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (localStorageValue) {
+    const { email, message } = localStorageValue;
+    const { email: emailForm, message: messageForm } = formElement.elements;
+    emailForm.value = email || '';
+    messageForm.value = message || '';
+  }
+}
+
+showDataForm();
